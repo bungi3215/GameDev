06dcccb483c916561d3a43abb70903de238e34d3
Søren Pedersen <sp@vicatheq.dk>
2018-07-12 20:55:31 +0200

    Added ping counter

---
+++ Testing/Assets/Scripts/MultiplayerScripts/PingManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PingManager : Photon.MonoBehaviour {
+
+	public Text pingText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		int ping = PhotonNetwork.networkingPeer.RoundTripTime;
+
+		pingText.text = ping.ToString();
+
+	}
+}
