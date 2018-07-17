511f5440b4a48df7c68999e4547454fd0eb5edc9
Søren Pedersen <sp@vicatheq.dk>
2018-07-17 21:27:30 +0200

    Ship animations no longer plays, but does not play at all on other ship.

--- Testing/Assets/Scripts/VehicleScripts/shipAnimations.cs
+++ Testing/Assets/Scripts/VehicleScripts/shipAnimations.cs
@@ -2,83 +2,100 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class shipAnimations : Photon.MonoBehaviour{
+public class shipAnimations : Photon.MonoBehaviour
+{
 
-    public ParticleSystem thrustersForward;
-    public ParticleSystem thrustersBackward;
-    public ParticleSystem thrustersTop;
-    public ParticleSystem thrustersBottom;
-    public ParticleSystem thrustersLeft;
-    public ParticleSystem thrustersRight;
-    private GameObject gameData;
+	public ParticleSystem thrustersForward;
+	public ParticleSystem thrustersBackward;
+	public ParticleSystem thrustersTop;
+	public ParticleSystem thrustersBottom;
+	public ParticleSystem thrustersLeft;
+	public ParticleSystem thrustersRight;
+	private GameObject gameData;
 
-    // Update is called once per frame
+	// Update is called once per frame
 
-    void Update () {
 
-		//if (!photonView.isMine)
-		//{
-		//	return;
-		//}
 
-        var inside = GameManager.isInsideVehicle;
+	private void Start()
+	{
+		thrustersForward.Stop();
+		thrustersBackward.Stop();
+		thrustersTop.Stop();
+		thrustersBottom.Stop();
+		thrustersRight.Stop();
+		thrustersLeft.Stop();
 
-        if(inside == true)
-        {
-            var verticalThruster = Input.GetAxis("Vertical");
-            var HorizontalThruster = Input.GetAxis("Horizontal");
-            var lateralThrusters = Input.GetAxis("lift");
+	}
 
-            /**
+
+	void Update()
+	{
+
+		if (!photonView.isMine)
+		{
+			return;
+		}
+
+		var inside = GameManager.isInsideVehicle;
+
+		if (inside == true)
+		{
+			var verticalThruster = Input.GetAxis("Vertical");
+			var HorizontalThruster = Input.GetAxis("Horizontal");
+			var lateralThrusters = Input.GetAxis("lift");
+
+			/**
              * Forward / backward
             **/
-            if (verticalThruster > 0)
-            {
-                thrustersForward.Play();
-            }
-            else if (verticalThruster < 0)
-            {
-                thrustersBackward.Play();
-            }
-            else {
-                thrustersForward.Stop();
-                thrustersBackward.Stop();
-            }
+			if (verticalThruster > 0)
+			{
+				thrustersForward.Play();
+			}
+			else if (verticalThruster < 0)
+			{
+				thrustersBackward.Play();
+			}
+			else
+			{
+				thrustersForward.Stop();
+				thrustersBackward.Stop();
+			}
 
-            /**
+			/**
              * Up / Down
             **/
-            
-            if (lateralThrusters > 0)
-            {
-                thrustersBottom.Play();
-            }
-            else if (lateralThrusters < 0)
-            {
-                thrustersTop.Play();
-            }
-            else
-            {
-                thrustersTop.Stop();
-                thrustersBottom.Stop();
-            }
 
-            /**
+			if (lateralThrusters > 0)
+			{
+				thrustersBottom.Play();
+			}
+			else if (lateralThrusters < 0)
+			{
+				thrustersTop.Play();
+			}
+			else
+			{
+				thrustersTop.Stop();
+				thrustersBottom.Stop();
+			}
+
+			/**
              * Sides
             **/
-            if (HorizontalThruster > 0)
-            {
-                thrustersLeft.Play();
-            }
-            else if (HorizontalThruster < 0)
-            {
-                thrustersRight.Play();
-            }
-            else
-            {
-                thrustersRight.Stop();
-                thrustersLeft.Stop();
-            }
-        }
-    }
+			if (HorizontalThruster > 0)
+			{
+				thrustersLeft.Play();
+			}
+			else if (HorizontalThruster < 0)
+			{
+				thrustersRight.Play();
+			}
+			else
+			{
+				thrustersRight.Stop();
+				thrustersLeft.Stop();
+			}
+		}
+	}
 }
